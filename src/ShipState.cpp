73709f39a524bad1f0263fabc8911df716cf1785
Eviatar Davidyan <eviatarda@std.hac.ac.il>
2023-06-07 20:54:49 +0300

    save

--- src/ShipState.cpp
+++ src/ShipState.cpp
@@ -6,33 +6,40 @@ void ShipState::move(Direction direction, b2Body* body)
     {
     case Up:
     {
-        if (true)//to change
-        {
-            body->ApplyLinearImpulseToCenter(b2Vec2(0.0f, -SHIP_JUMP_FORCE), true);
-        }
+        float currentSpeed_y = body->GetLinearVelocity().y;
+        body->SetLinearVelocity({ MOVEMENT_SPEED, currentSpeed_y });
+        body->ApplyLinearImpulseToCenter(b2Vec2(0.0f, -SHIP_JUMP_FORCE), true);
         break;
     }
-    case Right:
+    default:
     {
         float currentSpeed_y = body->GetLinearVelocity().y;
         body->SetLinearVelocity({ MOVEMENT_SPEED, currentSpeed_y });
         break;
-    }
-    case Left:
-    {
-        float currentSpeed_y = body->GetLinearVelocity().y;
-        body->SetLinearVelocity({ -MOVEMENT_SPEED, currentSpeed_y });
-        break;
-    }
 
-    case Stay:
-    {
-        float currentSpeed_y = body->GetLinearVelocity().y;
-        body->SetLinearVelocity({ 0, currentSpeed_y });
-        break;
     }
+}
+    //case Right:
+    //{
+    //    float currentSpeed_y = body->GetLinearVelocity().y;
+    //    body->SetLinearVelocity({ MOVEMENT_SPEED, currentSpeed_y });
+    //    break;
+    //}
+    //case Left:
+    //{
+    //    float currentSpeed_y = body->GetLinearVelocity().y;
+    //    body->SetLinearVelocity({ -MOVEMENT_SPEED, currentSpeed_y });
+    //    break;
+    //}
 
-    default:
-        break;
-    }
+    //case Stay:
+    //{
+    //    float currentSpeed_y = body->GetLinearVelocity().y;
+    //    body->SetLinearVelocity({ 0, currentSpeed_y });
+    //    break;
+    //}
+
+    //default:
+    //    break;
+    //}
 }
